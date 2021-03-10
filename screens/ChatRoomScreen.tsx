4ebc9f5135c98daf6ba6f574d372d1898f36b555
Tom Wennerström <teewee@mbnet.fi>
2021-03-10 16:38:07 +0200

    Display and fetch messages

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, ImageBackground } from 'react-native';
-
 import { useRoute } from '@react-navigation/native';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
 
+import { messagesByChatRoom } from '../src/graphql/queries';
 import chatRoomData from '../data/Chats';
 import ChatMessage from '../components/ChatMessage';
 import BG from '../assets/images/BG.png';
@@ -10,13 +11,40 @@ import InputBox from "../components/InputBox";
 
 const ChatRoomScreen = () => {
 
+    const [messages, setMessages] = useState([]);
+    const [myID, setMyID] = useState(null);
+
     const route = useRoute();
+    console.log(route.params.id);
+
+    useEffect( () => {
+        const fetchMessages = async () => {
+            const messagesData = await API.graphql(
+                graphqlOperation(
+                    messagesByChatRoom, {
+                        chatRoomID: route.params.id,
+                        sortDirection: "DESC",
+                    }
+                )
+            )
+            setMessages(messagesData.data.messagesByChatRoom.items);
+        }
+        fetchMessages();
+    }, [])
+
+    useEffect(() => {
+        const getMyID = async () => {
+            const userInfo = await Auth.currentAuthenticatedUser();
+            setMyID(userInfo.attributes.sub);
+        }
+        getMyID();
+    }, [])
 
     return (
         <ImageBackground style={{width: '100%', height: '100%'}}  source={BG}>
             <FlatList 
-                data={chatRoomData.messages}
-                renderItem={({ item }) => <ChatMessage message={item} />}
+                data={messages}
+                renderItem={({ item }) => <ChatMessage myID={myID} message={item} />}
                 inverted
                 keyExtractor={(item) => item.id}
             />
