f73e22ba6427300993a588fea11d18c1e5042a8b
Tom Wennerström <teewee@mbnet.fi>
2021-03-10 13:13:45 +0200

    GraphQL API - User

--- App.tsx
+++ App.tsx
@@ -1,19 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-import { withAuthenticator } from 'aws-amplify-react-native'
-import Amplify from 'aws-amplify'
-import config from './src/aws-exports'
+import { getUser } from './src/graphql/queries';
+import { createUser } from './src/graphql/mutations';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import Amplify, { Auth, API, graphqlOperation } from 'aws-amplify';
+import config from './src/aws-exports';
 Amplify.configure(config)
+
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-8.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+]
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages [Math.floor(Math.random() * randomImages.length)]
+  }
+
+  useEffect ( () => {
+    const fetchUser = async () => {
+      // Get Authenticated user
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+
+      if (userInfo) {
+      // Get the user from Backend with user IF from Auth
+        const userData = await API.graphql(
+          graphqlOperation(
+            getUser,
+            {id: userInfo.attributes.sub}
+          )
+        )
+
+        if (userData.data.getUser) {
+          console.log("User already registered");
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: 'Hey, I am using Chatty!', 
+        }
+        console.log(newUser);
+
+        await API.graphql(
+          graphqlOperation(
+            createUser,
+            {input: newUser}
+          )
+        )
+
+      // if no user in DB with id, create one
+      }
+    }
+      fetchUser();
+  }, [])
 
   if (!isLoadingComplete) {
     return null;
